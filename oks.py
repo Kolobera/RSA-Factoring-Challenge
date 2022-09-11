9f1289f9e11f735299a3b4bc9779fa0c7743c92f
Ousmane Konate <konatealim@gmail.com>
2022-09-11 19:57:02 +0200

    aad

--- oks.py
+++ oks.py
@@ -1,6 +1,6 @@
 lp = []
 
-with open('2T_part1.txt', 'r') as file:
+"""with open('2T_part1.txt', 'r') as file:
     lines = file.readlines()
     for i in lines:
         for j in list(i.split()):
@@ -9,5 +9,12 @@ with open("prime.txt", "w") as file:
     for i in lp:
         if i < 15000000:
             file.write(str(i)+",")
+"""
+from functools import reduce
+from math import sqrt
 
-
+def factors_1(n):
+    step = 2 if n%2 else 1
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
+print(factors_1(2497885147362973))
--- prime.py
+++ prime.py
@@ -6,6 +6,13 @@ with open("prime.txt", 'r') as file:
             break
 
 import itertools
+from functools import reduce
+from math import sqrt
+def factors_1(n):
+    step = 2 if n%2 else 1
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
+
 
 def isprime(n):
     if n % 2 == 0:
