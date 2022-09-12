e8ed04828e96321002f68d98a1511332798ba991
Ousmane Konate <konatealim@gmail.com>
2022-09-13 00:09:18 +0200

    aad

--- prime.py
+++ prime.py
@@ -12,15 +12,17 @@ def factors_1(n):
     return set(reduce(list.__add__,
                 ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
 
-
-"""def isprime(n):
-    if n % 2 == 0:
-        return 2
-    elif n % 3 == 0:
-        return 3
-    divs = range(5, int(n ** 0.5) + 1, 2)
+import itertools
+def isprime(n):
+    for i in lp:
+        j = int(i)
+        if n % j == 0:
+            if j == n:
+                return 1
+            return j
+    divs = range(1000001, int(n ** 0.5) + 1, 2)
     return [d for d in itertools.chain(divs[::3], divs[1::3]) if n % d == 0][0]
-"""
+
 def factor(n):
     for i in lp:
         j = int(i)
@@ -36,30 +38,30 @@ def factor(n):
     l = [d for d in itertools.chain(divs[::3], divs[1::3]) if n % d == 0]
     if l != []:
         return l[0]
-    k = 166660
+    k = 1666660
     while 6*k+1 <= int(n**0.5) + 1:
         if n % (6*k+1) == 0:
             return 6*k+1
         if n % (6*k-1) == 0:
             return 6*k-1"""
 
+    """
     if n > 10000000:
         
-        li=[(6*i+1, 6*i-1) for i in range(2499999, int((int(n**.5) + 1)/6) + 1) if (n % (6*i+1) == 0 or n % (6*i-1) == 0)]
+        li=[(6*i+1, 6*i-1) for i in range(1666660, int((int(n**.5) + 1)/6) + 1) if (n % (6*i+1) == 0 or n % (6*i-1) == 0)]
         #li = set(reduce(list.__add__,
         #        ((6*i+1, 6*i-1) for i in range(1666660, int((int(n**.5) + 1)/6) + 1) if (n % (6*i+1) == 0 or n % (6*i-1) == 0))))
         if li != []:
             for i in li[0]:
                 if n % i == 0:
-                    return i
+                    return i"""
 
-        """for i in range(1666660, int((int(n**.5) + 1)/6) + 1):
+    for i in range(1666660, int((int(n**.5) + 1)/6) + 1):
             if n % (6*i+1) == 0:
-                print("ijoij")
                 return 6*i+1
             if n % (6*i-1) == 0:
                 return 6*i-1
-        li=[(6*i+1, 6*i-1) for i in range(1666660, int((int(n**.5) + 1)/6) + 1) if (n % (6*i+1) == 0, n % (6*i-1) == 0)]"""
+        
     return 1
 def primef(n):
     if n <= 3:
