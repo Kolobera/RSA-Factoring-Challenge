79a02cba6e9f8fcd1a7c50ad34ca3dcafb0ac9e6
Ousmane Konate <konatealim@gmail.com>
2022-09-18 02:08:19 +0200

    add

--- oks.py
+++ oks.py
@@ -1,4 +1,4 @@
-lp = []
+"""lp = []
 
 with open('2T_part1.txt', 'r') as file:
     lines = file.readlines()
@@ -9,7 +9,6 @@ with open("prime.txt", "w") as file:
     for i in lp:
         if i < 10000000:
             file.write(str(i)+",")
-"""
 from functools import reduce
 from math import sqrt
 
@@ -18,3 +17,41 @@ def factors_1(n):
     return set(reduce(list.__add__,
                 ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
 print(factors_1(2497885147362973))"""
+def gen_primes(n):
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+    
+    # The running integer that's checked for primeness
+    q = 2
+    
+    while q<n:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            # 
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next 
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            # 
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        if n % int(q) == 0:
+            print('zzzzé', int(q))
+            return q
+        q += 1
+
+j = [x for x in gen_primes(239821585064027) if 239821585064027 % x == 0]
+print(j)
--- prime.py
+++ prime.py
@@ -19,6 +19,46 @@ def isprime(n,lp):
     divs = range(1000001, int(n ** 0.5) + 1, 2)
     return [d for d in itertools.chain(divs[::3], divs[1::3]) if n % d == 0][0]
 
+
+def gen_primes(n):
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+    
+    # The running integer that's checked for primeness
+    q = 2
+    
+    while q<n:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            # 
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next 
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            # 
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        if n % int(q) == 0:
+            break
+        
+        
+        q += 1
+
+
+
 def factor(n, lp):
     if n in lp:
         return 1
